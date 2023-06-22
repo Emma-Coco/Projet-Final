e59a14c9b6e3d2d404b3e68f3124fed7c1f12b70
EmmaCoco6 <emma.coco.pro@gmail.com>
2023-06-22 21:18:10 +0200

    corrections

--- controlleurs/CreateAnnouncement.php
+++ controlleurs/CreateAnnouncement.php
@@ -7,8 +7,8 @@ echo '<pre>';
 var_dump($_REQUEST);
 echo '</pre>';
 
-$query = "INSERT INTO logement (name, description, position_lat, position_long, adress, number_of_travelers, price, id_type_logement, bedroom_number, kitchen, bathroom_number, created_at) ";
-$query .= "VALUES (:name, :description, :position_lat, :position_long, :adress, :number_of_travelers, :price, :id_type_logement, :bedroom_number, :kitchen, :bathroom_number, :created_at)";
+$query = "INSERT INTO logement (name, description, position_lat, position_long, adress, number_of_travelers, price, id_type_logement, bedroom_number, kitchen, bathroom_number) ";
+$query .= "VALUES (:name, :description, :position_lat, :position_long, :adress, :number_of_travelers, :price, :id_type_logement, :bedroom_number, :kitchen, :bathroom_number)";
 $stmt = $con->prepare($query);
 $stmt->bindParam(':name', $_REQUEST["nom"]);
 $stmt->bindParam(':description', $_REQUEST["description"]);
@@ -21,9 +21,8 @@ $stmt->bindParam(':id_type_logement', $_REQUEST["typeLogement"]);
 $stmt->bindParam(':bedroom_number', $_REQUEST["chambres"]);
 $stmt->bindParam(':kitchen', $_REQUEST["cuisine"]);
 $stmt->bindParam(':bathroom_number', $_REQUEST["sallesDeBain"]);
-$stmt->bindParam(':created_at', $_REQUEST["dateCreation"]);
 $stmt->execute();
-$logement_id = $con->lastInsertId();
+$id_logement = $con->lastInsertId();
 
 //ajout des services
 
@@ -31,14 +30,12 @@ $con = DBConnexion::getDBConnexion();
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $services = $_POST["services"];
-    $created_at = $_POST["created_at"];
 
     foreach ($services as $service_id) {
-        $query = "INSERT INTO service_logement (id_service, id_logement, created_at) VALUES (:id_service, :id_logement, :created_at)";
+        $query = "INSERT INTO service_logement (id_service, id_logement) VALUES (:id_service, :id_logement)";
         $stmt = $con->prepare($query);
         $stmt->bindParam(':id_service', $service_id);
-        $stmt->bindParam(':id_logement', $logement_id);
-        $stmt->bindParam(':created_at', $created_at);
+        $stmt->bindParam(':id_logement', $id_logement);
 
         if ($stmt->execute()) {
             echo "Service ajouté avec succès : Service ID = " . $service_id . "<br>";
@@ -57,16 +54,17 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         // Connexion réussie
 
         // Vérifier si le répertoire existe
-        if (is_dir($directory)) {
-            $images = getImagesFromDirectory($directory);
+        //if (is_dir($directory)) {
+            //$images = getImagesFromDirectory($directory);
 
             // Insérer les URL des images dans la table "picture" avec l'ID du logement
-            foreach ($images as $image) {
+            //foreach ($images as $image) {
                 $sql = "INSERT INTO picture (url, id_logement) VALUES (:url, :id_logement)";
                 $stmt = $con->prepare($sql);
-                $stmt->bindParam(':url', $image);
+                $stmt->bindParam(':url', $directory);
                 $stmt->bindParam(':id_logement', $id_logement);
-                if ($stmt->execute()) {
+                $stmt->execute();
+                /*if ($stmt->execute()) {
                     echo "URL insérée avec succès : " . $image . "<br>";
                 } else {
                     echo "Erreur lors de l'insertion de l'URL : " . $stmt->error . "<br>";
@@ -74,7 +72,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             }
         } else {
             echo "Répertoire invalide.";
-        }
+        }*/
     } else {
         die("Échec de la connexion à la base de données.");
     }
--- vue/CreateAnnouncement.php
+++ vue/CreateAnnouncement.php
@@ -7,83 +7,83 @@ include_once('../modele/TypeLogements.php');
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Formulaire</title>
 </head>
+
 <body>
 
-<h1>Ajouter un logement<h1>
+    <h1>Ajouter un logement<h1>
 
-<h2>Description du logement</h2>
+            <h2>Description du logement</h2>
 
-    <form action="../controlleurs/CreateAnnouncement.php" method="POST">
-        <label for="nom">Nom :</label>
-        <input type="text" id="nom" name="nom" required placeholder="Entrez le nom"><br><br>
+            <form action="../controlleurs/CreateAnnouncement.php" method="POST">
+                <label for="nom">Nom :</label>
+                <input type="text" id="nom" name="nom" required placeholder="Entrez le nom"><br><br>
 
-        <label for="description">Description :</label>
-        <textarea id="description" name="description" rows="4" cols="50" required placeholder="Décrivez l'hébergement"></textarea><br><br>
+                <label for="description">Description :</label>
+                <textarea id="description" name="description" rows="4" cols="50" required
+                    placeholder="Décrivez l'hébergement"></textarea><br><br>
 
-        <label for="latitude">Latitude :</label>
-        <input type="text" id="latitude" name="latitude" required placeholder="Ex: 48.8566"><br><br>
+                <label for="latitude">Latitude :</label>
+                <input type="text" id="latitude" name="latitude" required placeholder="Ex: 48.8566"><br><br>
 
-        <label for="longitude">Longitude :</label>
-        <input type="text" id="longitude" name="longitude" required placeholder="Ex: 2.3522"><br><br>
+                <label for="longitude">Longitude :</label>
+                <input type="text" id="longitude" name="longitude" required placeholder="Ex: 2.3522"><br><br>
 
-        <label for="adresse">Adresse :</label>
-        <input type="text" id="adresse" name="adresse" required placeholder="Entrez l'adresse"><br><br>
+                <label for="adresse">Adresse :</label>
+                <input type="text" id="adresse" name="adresse" required placeholder="Entrez l'adresse"><br><br>
 
-        <label for="voyageurs">Nombre de voyageurs :</label>
-        <input type="number" id="voyageurs" name="voyageurs" required placeholder="Entrez le nombre de voyageurs"><br><br>
+                <label for="voyageurs">Nombre de voyageurs :</label>
+                <input type="number" id="voyageurs" name="voyageurs" required
+                    placeholder="Entrez le nombre de voyageurs"><br><br>
 
-        <label for="prix">Prix :</label>
-        <input type="number" id="prix" name="prix" required placeholder="Entrez le prix"><br><br>
+                <label for="prix">Prix :</label>
+                <input type="number" id="prix" name="prix" required placeholder="Entrez le prix"><br><br>
 
-        <label for="typeLogement">Type de logement :</label>
-        <select name="typeLogement" id="typeLogement">
-            <?php
-            print(typeLogement::getTypeLogements());
-            ?>
-        </select><br><br>
+                <label for="typeLogement">Type de logement :</label>
+                <select name="typeLogement" id="typeLogement">
+                    <?php
+                    print(typeLogement::getTypeLogements());
+                    ?>
+                </select><br><br>
 
-        <label for="chambres">Nombre de chambres :</label>
-        <input type="number" id="chambres" name="chambres" required placeholder="Entrez le nombre de chambres"><br><br>
+                <label for="chambres">Nombre de chambres :</label>
+                <input type="number" id="chambres" name="chambres" required
+                    placeholder="Entrez le nombre de chambres"><br><br>
 
-        <label for="cuisine">Cuisine :</label>
-        <input type="text" id="cuisine" name="cuisine" required placeholder="Indiquez s'il y a une cuisine"><br><br>
+                <label for="cuisine">Cuisine :</label>
+                <input type="text" id="cuisine" name="cuisine" required
+                    placeholder="Indiquez s'il y a une cuisine"><br><br>
 
-        <label for="sallesDeBain">Nombre de salles de bain :</label>
-        <input type="number" id="sallesDeBain" name="sallesDeBain" required placeholder="Entrez le nombre de salles de bain"><br><br>
+                <label for="sallesDeBain">Nombre de salles de bain :</label>
+                <input type="number" id="sallesDeBain" name="sallesDeBain" required
+                    placeholder="Entrez le nombre de salles de bain"><br><br>
 
-        <label for="dateCreation">Créé à :</label>
-        <input type="date" id="dateCreation" name="dateCreation" required><br><br>
+                <!--ajout des services-->
 
-        <!--ajout des services-->
+                <h2>Ajouter des services au logement</h2>
 
-<h2>Ajouter des services au logement</h2>
+                <h3>Services disponibles :</h3>
 
-        <h3>Services disponibles :</h3>
+                <?php
 
-        <?php
+                typeLogement::GetAllService();
 
-        typeLogement::GetAllService();
-        
-        ?><br>
-
-        <label for="created_at">Date de création :</label>
-        <input type="date" id="created_at" name="created_at" required><br><br>
-
+                ?><br>
 
 
-<h2>Ajouter des images</h2>
-    
-        <!--formulaire de récupération des url images-->
-        <label for="directory">Répertoire :</label>
-        <input type="text" id="directory" name="directory" placeholder="Chemin du répertoire" required>
-        <br>
+                <h2>Ajouter des images</h2>
+
+                <!--formulaire de récupération des url images-->
+                <label for="directory">Image principale :</label>
+                <input type="text" id="directory" name="directory" placeholder="Nom_Du_Dossier/url.jpg" required>
+                <br>
 
 
-        <input type="submit" value="Envoyer"><br><br>
-    </form>
+                <input type="submit" value="Envoyer"><br><br>
+            </form>
 </body>
-</html>
 
+</html>
