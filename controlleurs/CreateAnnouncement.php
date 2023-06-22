abb74c933f598eebc6b2b931f49b817b03411ebc
EmmaCoco6 <emma.coco.pro@gmail.com>
2023-06-22 20:30:24 +0200

    createannouncement

--- controlleurs/CreateAnnouncement.php
+++ controlleurs/CreateAnnouncement.php
@@ -23,7 +23,7 @@ $stmt->bindParam(':kitchen', $_REQUEST["cuisine"]);
 $stmt->bindParam(':bathroom_number', $_REQUEST["sallesDeBain"]);
 $stmt->bindParam(':created_at', $_REQUEST["dateCreation"]);
 $stmt->execute();
-$id_logement = $con->lastInsertId();
+$logement_id = $con->lastInsertId();
 
 //ajout des services
 
@@ -37,7 +37,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $query = "INSERT INTO service_logement (id_service, id_logement, created_at) VALUES (:id_service, :id_logement, :created_at)";
         $stmt = $con->prepare($query);
         $stmt->bindParam(':id_service', $service_id);
-        $stmt->bindParam(':id_logement', $id_logement);
+        $stmt->bindParam(':id_logement', $logement_id);
         $stmt->bindParam(':created_at', $created_at);
 
         if ($stmt->execute()) {
@@ -48,41 +48,25 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
 }
 
-// Charger un dossier d'images, et sélectionner l'image principale
+// Récupérer les images, de manière récursive, incluses dans chaque sous-dossier
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $directory = $_POST["directory"];
-
-    
 
     if ($con) {
         // Connexion réussie
 
         // Vérifier si le répertoire existe
-        //if (is_dir($directory)) {
-            //$images = getImagesFromDirectory($directory);
+        if (is_dir($directory)) {
+            $images = getImagesFromDirectory($directory);
 
             // Insérer les URL des images dans la table "picture" avec l'ID du logement
-            //foreach ($images as $image) {
-                $directory = $_POST['directory'];
-                $targetDirectory = '../vue/Images/Appartements_images/';
-
-                if (!empty($_FILES['images']['name'])) {
-                    $targetPath = $targetDirectory . basename($_FILES['images']['name']);
-                    if (move_uploaded_file($_FILES['images']['tmp_name'], $targetPath)) {
-                        // Téléchargement réussi, vous pouvez effectuer d'autres traitements si nécessaire
-                        echo "Le dossier d'images a été téléchargé et stocké avec succès.";
-                    } else {
-                        echo "Une erreur s'est produite lors du téléchargement du dossier d'images.";
-                    }
-                }
-
+            foreach ($images as $image) {
                 $sql = "INSERT INTO picture (url, id_logement) VALUES (:url, :id_logement)";
                 $stmt = $con->prepare($sql);
-                $stmt->bindParam(':url', $directory);
+                $stmt->bindParam(':url', $image);
                 $stmt->bindParam(':id_logement', $id_logement);
-                $stmt->execute();
-                /*if ($stmt->execute()) {
+                if ($stmt->execute()) {
                     echo "URL insérée avec succès : " . $image . "<br>";
                 } else {
                     echo "Erreur lors de l'insertion de l'URL : " . $stmt->error . "<br>";
@@ -90,7 +74,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             }
         } else {
             echo "Répertoire invalide.";
-        }*/
+        }
     } else {
         die("Échec de la connexion à la base de données.");
     }
